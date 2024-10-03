712411b2aeb8ed72aeac726bd1bf4006b5f9269b
marc.rodriguez.gonzalez <marc.rodriguez.gonzalez@auleseebe.upc.edu>
2024-10-03 11:06:34 +0200

    h

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,10 @@
 #Codigo para problema 2
+
+mis_dades <- iris
+
+linreg <- lm(Sepal.Length ~ Petal.Length, data = mis_dades)
+predict(linreg, data.frame(Petal.Length=1.5))
+cor.test(mis_dades$Petal.Length, mis_dades$Sepal.Length)
+predict(linreg, data.frame(Petal.Length=mis_dades$Petal.Length))
+
+plot(mis_dades$Petal.Length, mis_dades$Sepal.Length)
